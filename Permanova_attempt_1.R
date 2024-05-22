fb5708e71ed7eac061de032a2c174e38ed0103c2
itsmadiross <Rossm@sou.edu>
2024-05-22 15:00:14 -0700

    GH things

--- Permanova_attempt_1.R
+++ Permanova_attempt_1.R
@@ -5,6 +5,8 @@ library(here)
 ### Field data --------------
 field.data.raw <- read.csv(here("data/SRWC-seedbank_plant-survey_2024-02-26.csv"), header = TRUE)
 
+## Color 
+fun.palatte <- c("darkolivegreen","darkgoldenrod")
 
 # removing duplicate rows
 dup.1 <- which(field.data.raw$plot == "rf.6.a" & field.data.raw$species.code == "unkfb")[2]
@@ -43,14 +45,37 @@ nmdsPlot[,c('meadow','habitat','transect','plot')] <-
 
 ggplot(nmdsPlot, aes(x, y, shape=factor(meadow), color=factor(habitat))) +
   geom_point(size=2) + 
-  theme_bw() +
+  scale_color_manual(values = fun.palatte) +
+  theme_classic() +
   xlab('NMDS 1') +
   ylab('NMDS 2')
 
+ggsave(plot = nmdsPlot , filename = "figures/NMDS_species_field.data.png", width = 6, height = 4, units = "in")
 
 ## Greenhouse data ---------------
+greenhouse.sum.clean 
 
 # using greenhouse.sum.clean data from Rmd file
+
+# switching to wide format for vegan
+greenhouse.wide <- greenhouse.sum.clean %>%
+  pivot_wider(names_from = species.code, values_from = species.count, 
+              names_sort = TRUE, values_fill = 0)
+
+view(field.wide)
+
+##Permanova 
+
+# species matrix only
+sp.matrix <- field.wide %>% select(abicon:vioadu)
+
+perma.1 <- adonis2(sp.matrix ~ habitat + meadow, 
+                   data = field.wide, method ="jaccard")
+
+perma.1
+
+
+
 
 
 greenhouse.sum.clean
